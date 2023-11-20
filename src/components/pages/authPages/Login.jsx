c74d29f976e609e5fee7bf32a9dcceccceb6d75b
rifat-incrementsinc <rifat.amin@incrementsinc.com>
2023-11-20 18:18:27 +0600

    login page done

--- src/components/pages/authPages/Login.jsx
+++ src/components/pages/authPages/Login.jsx
@@ -1,7 +1,260 @@
 import React from 'react'
-
+import {
+    Grid,
+    TextField,
+    Typography,
+    Box,
+    Paper,
+    Button,
+    Checkbox,
+    InputAdornment,
+    IconButton,
+} from '@mui/material'
+import Stack from '@mui/material/Stack'
+import { useForm, Controller } from 'react-hook-form'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
+import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined'
+import { Link } from 'react-router-dom'
+import bgImage from '../../../assets/bg_tb.png'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
 const Login = () => {
-    return <div>Login</div>
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    const handleTogglePasswordVisibility = () => {
+        setShowPassword(!showPassword)
+    }
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        // Handle login logic here
+        console.log(data)
+    }
+
+    return (
+        <Stack
+            sx={{
+                backgroundImage: `url(${bgImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '100vh',
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Grid container>
+                {/* Left Side */}
+                <Grid item xs={12} sm={6}></Grid>
+
+                {/* Right Side */}
+                <Grid
+                    item
+                    md={6}
+                    lg={6}
+                    xs={12}
+                    sm={12}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        padding: '20px',
+                        transition: 'height 5s ease-in-out',
+                    }}
+                >
+                    <Stack
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            width: '450px',
+                            transition: 'height 5s ease-in-out',
+                        }}
+                    >
+                        <Stack
+                            component={Paper}
+                            sx={{
+                                backgroundColor: 'rgba(255, 255, 255, 0.85)',
+                                padding: '20px',
+                                borderRadius: '20px',
+                            }}
+                        >
+                            <Typography
+                                variant='captionLarge'
+                                color='textPrimary'
+                                textAlign='left'
+                                sx={{ mb: '16px' }}
+                            >
+                                Login
+                            </Typography>
+                            <Typography
+                                variant='bodyLarge'
+                                textAlign='left'
+                                sx={{
+                                    mb: '16px',
+                                }}
+                            >
+                                Enter your credentials to log in to your
+                                account.
+                            </Typography>
+
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div style={{ marginBottom: '10px' }}>
+                                    <Typography variant={'bodyMedium'}>
+                                        Email Address *
+                                    </Typography>
+                                    <Controller
+                                        name='email'
+                                        control={control}
+                                        defaultValue=''
+                                        render={({ field }) => (
+                                            <TextField
+                                                sx={{ mt: '8px' }}
+                                                fullWidth
+                                                placeholder={
+                                                    'eg. johndoe@example.com'
+                                                }
+                                                {...field}
+                                                error={!!errors.email}
+                                                helperText={
+                                                    errors.email
+                                                        ? 'Email is required'
+                                                        : ''
+                                                }
+                                                InputProps={{
+                                                    startAdornment: (
+                                                        <InputAdornment position='start'>
+                                                            <EmailOutlinedIcon />
+                                                        </InputAdornment>
+                                                    ),
+                                                }}
+                                            />
+                                        )}
+                                        rules={{ required: true }}
+                                    />
+                                </div>
+
+                                <div style={{ marginBottom: '10px' }}>
+                                    <Typography variant={'bodyMedium'}>
+                                        Password *
+                                    </Typography>
+                                    <Controller
+                                        name='password'
+                                        control={control}
+                                        defaultValue=''
+                                        render={({ field }) => (
+                                            <TextField
+                                                sx={{ mt: '8px' }}
+                                                fullWidth
+                                                type={
+                                                    showPassword
+                                                        ? 'text'
+                                                        : 'password'
+                                                }
+                                                placeholder={
+                                                    'Enter your password'
+                                                }
+                                                {...field}
+                                                error={!!errors.password}
+                                                helperText={
+                                                    errors.password
+                                                        ? 'Password is required'
+                                                        : ''
+                                                }
+                                                InputProps={{
+                                                    startAdornment: (
+                                                        <InputAdornment position='start'>
+                                                            <KeyOutlinedIcon />
+                                                        </InputAdornment>
+                                                    ),
+                                                    endAdornment: (
+                                                        <InputAdornment position='end'>
+                                                            <IconButton
+                                                                onClick={
+                                                                    handleTogglePasswordVisibility
+                                                                }
+                                                            >
+                                                                {showPassword ? (
+                                                                    <Visibility />
+                                                                ) : (
+                                                                    <VisibilityOff />
+                                                                )}
+                                                            </IconButton>
+                                                        </InputAdornment>
+                                                    ),
+                                                }}
+                                            />
+                                        )}
+                                        rules={{ required: true }}
+                                    />
+                                </div>
+
+                                <Button
+                                    variant='contained'
+                                    sx={{ width: '100%' }}
+                                    type='submit'
+                                >
+                                    Login
+                                </Button>
+                            </form>
+                            <div
+                                style={{
+                                    textAlign: 'center',
+                                    marginTop: '15px',
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        marginBottom: '10px',
+                                    }}
+                                >
+                                    <Typography
+                                        variant={'bodyLarge'}
+                                        color={'textSecondary'}
+                                    >
+                                        Don't have an account?{' '}
+                                        <Link
+                                            to={'/signup'}
+                                            style={{
+                                                textDecoration: 'none',
+                                                cursor: 'pointer',
+                                                color: 'inherit',
+                                            }}
+                                        >
+                                            Signup
+                                        </Link>
+                                    </Typography>
+                                </div>
+                                <div>
+                                    <Link
+                                        to='/forgot-password'
+                                        style={{ textDecoration: 'none' }}
+                                    >
+                                        <Typography
+                                            variant={'bodyLarge'}
+                                            color={'textSecondary'}
+                                            sx={{
+                                                cursor: 'pointer',
+                                                textDecoration: 'none',
+                                            }}
+                                        >
+                                            Forgot My Password
+                                        </Typography>
+                                    </Link>
+                                </div>
+                            </div>
+                        </Stack>
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Stack>
+    )
 }
 
 export default Login
